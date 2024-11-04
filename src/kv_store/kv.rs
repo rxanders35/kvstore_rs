df162a7928e5989690a969b50204f89eba7b6a0e
rxanders35 <reidxanders@outlook.com>
2024-11-04 00:48:35 -0500

    basic protobuf skeleton + core data structure

---
+++ src/kv_store/kv.rs
@@ -0,0 +1,43 @@
+use std::collections::HashMap;
+use std::sync::Arc;
+use std::hash::Hash;
+use tokio::sync::Mutex;
+
+enum OperationError {
+    /*
+    TODO:
+    - Implement different error types
+     */ 
+    KeyAlreadyExists, 
+    KeyNotFound,
+}
+
+struct KVDataStructure {
+    data: Arc<Mutex<HashMap<K, V>>>,
+}
+
+impl<K, V> KVDataStructure<K, V> where
+    K: Eq + Hash + Clone,
+    V: Clone,
+{
+
+    pub fn new() -> Self {
+        Self {
+            data: Arc::new(Mutex::new(HashMap::new())),
+        }
+    }
+
+    pub async fn put(&self, key: K, value: V) {
+        let mut map = self.data.lock().await; 
+        map.insert(key, value);
+    }
+
+    pub async fn get(&self, key: K) -> Option<V> {
+        let map = self.data.lock().await;
+        map.get(&key).cloned()
+    }
+
+    pub async fn delete(&self, key: K) -> Result<(), /* some error */> {
+        let mut map = self.data.lock().await;
+    }
+}
