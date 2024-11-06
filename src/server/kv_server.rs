d605838d23afc5e71d8656fe3dd939b67dacaa44
rxanders35 <reidxanders@outlook.com>
2024-11-05 19:52:43 -0500

    moved server logic to separate dir and file

---
+++ src/server/kv_server.rs
@@ -0,0 +1,19 @@
+use proto::*;
+use tonic::{transport::Server, Request, Response, Status};
+
+pub mod proto {
+    tonic::include_proto!("kv");
+}
+
+
+#[derive(Debug, Default)]
+struct KVService {}
+
+impl KVstore for KVService {
+   async fn put(
+    &self, 
+    request: Request<PutRq>
+    ) -> Result<Response<PutResp>, Status> {
+        // TODO: Implement 
+   } 
+}
