314a69a472fcad050aa14ab2f659b3bafa16ee8c
rxanders35 <reidxanders@outlook.com>
2024-11-05 19:47:38 -0500

    revised proto, created build.rs, beginnings of kv server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use proto::*;
+use tonic::{transport::Server, Request, Response, Status};
+
+pub mod proto {
+    tonic::include_proto!("kv");
+}
+
+
+#[derive(Debug, Default)]
+struct KVService {}
+
+impl KVstore for KVService {
+   async fn put(
+    &self, 
+    request: Request<PutRq>
+    ) -> Result<Response<PutResp>, Status> {
+        // TODO: Implement 
+   } 
+}
+
 fn main() {
     println!("Hello, world!");
 }
